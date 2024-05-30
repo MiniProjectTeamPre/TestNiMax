4fca935a321ed19b286980a540b01c66041af852
AnochaPay <Anocha_ping@hotmail.com>
2024-05-30 15:58:09 +0700

    add

---
+++ TestNiMax/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestNiMax
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public Ivi.Visa.Interop.ResourceManager listNi;
+        public OSC osc = new OSC();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listNi = new Ivi.Visa.Interop.ResourceManager();
+            try
+            {
+                string[] allNi = listNi.FindRsrc("?*");
+                foreach (string list_ in allNi)
+                {
+                    comboBox1.Items.Add(list_);
+                }
+            }catch { }
+            
+        }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            osc.name = comboBox1.Text;
+        }
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode != Keys.Enter)
+            {
+                return;
+            }
+
+            osc.Connect();
+            osc.Send(textBox1.Text);
+            osc.DisConnect();
+        }
+        private void textBox3_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode != Keys.Enter)
+            {
+                return;
+            }
+
+            osc.Connect();
+            textBox2.Text = osc.Query(textBox3.Text);
+            osc.DisConnect();
+        }
+
+
+
+        public class OSC
+        {
+            private Connect psu { get; set; }
+            public string name { get; set; }
+            
+
+            public OSC()
+            {
+                psu = new Connect();
+            }
+            public void Connect()
+            {
+                psu.ConnectInstr(name);
+            }
+            public void DisConnect()
+            {
+                psu.DisConnectInstr();
+            }
+            public void Send(string cmd)
+            {
+                psu.SendSLICCmd(cmd);
+            }
+            public string Query(string cmd)
+            {
+                return psu.QueryString(cmd);
+            }
+        }
+    }
+}
---
+++ VisaNi/VisaNi/Program.cs
@@ -0,0 +1,76 @@
+﻿using Ivi.Visa.Interop;
+using System;
+
+namespace VisaNi {
+    internal class Program {
+        static void Main(string[] args) {
+            string visaAddress = string.Empty;
+            string command = string.Empty;
+
+            if (args.Length < 2)
+            {
+                //Console.WriteLine("COM port parameter is missing.");
+                // Environment.Exit(1); // ถ้าคุณต้องการออกจากโปรแกรม
+                
+                //visaAddress = "USB0::0x05FF::0xEE3A::LCRY2150C02023::INSTR";
+                //command = "*IDN?";
+                //command = "TRig_MoDe SINGLE";
+                //command = "TRig_MoDe STOP";
+                //command = "TRig_MoDe?";
+            }
+            else
+            {
+                visaAddress = args[0];
+                command = args[1];
+            }
+
+            // สร้างอินสแตนซ์ของ VisaResourceManager
+            ResourceManager rm = new ResourceManager();
+
+            // เปิดการเชื่อมต่อกับอุปกรณ์ผ่าน Visa
+            FormattedIO488 visaDevice = new FormattedIO488();
+
+            try
+            {
+                if (string.IsNullOrEmpty(visaAddress))
+                {
+                    string[] visaList = rm.FindRsrc("?*");
+                    foreach (string list in visaList)
+                    {
+                        Console.WriteLine(list);
+                    }
+                }
+                else
+                {
+                    // เปิดการเชื่อมต่อกับอุปกรณ์ที่มีที่อยู่ VISA
+                    visaDevice.IO = (IMessage)rm.Open(visaAddress, AccessMode.NO_LOCK, 2000, "");
+
+                    // ส่งคำสั่งไปยังอุปกรณ์
+                    visaDevice.WriteString(command, true);
+
+                    // รับข้อมูลจากอุปกรณ์
+                    string response = "No Error";
+                    if (command.Contains("?"))
+                    {
+                        response = visaDevice.ReadString();
+                    }
+
+                    // แสดงผลลัพธ์
+                    Console.Write(response);
+                }
+            } catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred: " + ex.Message);
+            } finally
+            {
+                // ปิดการเชื่อมต่ออุปกรณ์
+                if (!string.IsNullOrEmpty(visaAddress))
+                {
+                    visaDevice.IO.Close();
+                }
+            }
+
+            //Console.ReadLine();
+        }
+    }
+}
